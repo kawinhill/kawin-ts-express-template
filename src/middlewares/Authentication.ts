3640cb39125ee62d9f36c1c6881e53c314e117a5
kawinhill1112 <kawinhill1112@gmail.com>
2024-03-04 09:35:06 +0700

    Add token validation in Authentication middleware

--- src/middlewares/Authentication.ts
+++ src/middlewares/Authentication.ts
@@ -6,6 +6,10 @@ import API_ERROR from "../libs/API_ERROR";
 export default class Authentication {
     public static async authenticate(req: any, res: any, next: any) {
         let token = req.headers.authorization;
+        // check if first 7 characters are 'Bearer ' (IF NOT THEN RETURN UNAUTHORIZED)
+        if (!token || token.length < 7 || token.substring(0, 7) !== 'Bearer ') {
+            return res.status(HTTP_STATUS.UNAUTHORIZED).send(API_ERROR.UNAUTHORIZED);
+        }
         token = token.replace('Bearer ', ''); 
         if (!token) {
             return res.status(HTTP_STATUS.UNAUTHORIZED).send(API_ERROR.UNAUTHORIZED);
