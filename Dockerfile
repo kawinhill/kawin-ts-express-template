847cbe6a1f3b1bf31d96e06f9490dbc1ca8b1529
kawinhill <31916065+kawinhill@users.noreply.github.com>
2024-03-29 22:07:40 +0700

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:20.11.1 as build
+WORKDIR /app
+
+COPY . .
+ENV NODE_ENV=development
+# PNPM
+RUN npm install -g pnpm
+RUN pnpm install
+RUN pnpm run build
+
+
+FROM node:20.11.1
+WORKDIR /app
+RUN npm install -g pnpm
+COPY --from=build /app/package.json /app/package.json
+RUN pnpm install
+COPY --from=build /app/dist /app/dist
+COPY --from=build /app/prisma /app/prisma
+COPY --from=build /app/docs /app/docs
+RUN pnpm prisma generate
+EXPOSE 3000
+CMD ["pnpm", "run", "start"]
--- src/routes/API/v1.ts
+++ src/routes/API/v1.ts
@@ -2,12 +2,14 @@ import * as express from "express";
 import Login from "../../controllers/API/public/Login";
 import Register from "../../controllers/API/public/Register";
 import Logout from "../../controllers/API/authenticated/Logout";
+import Me from "../../controllers/API/authenticated/Me";
 
 const router = express.Router();
 
 router.post('/login', Login.post);
 router.post('/register', Register.post);
 router.post('/logout', Authentication.authenticate, Logout.post);
+router.get('/me', Authentication.authenticate, Me.get);
 
 
 
