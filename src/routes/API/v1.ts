aca8cec00764287d348787c56b43710717c871d8
kawinhill <31916065+kawinhill@users.noreply.github.com>
2024-03-29 12:45:40 +0700

    add logout

--- src/routes/API/v1.ts
+++ src/routes/API/v1.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/login', Login.post);
 router.post('/register', Register.post);
+router.post('/logout', Authentication.authenticate, Logout.post);
 
 
 
