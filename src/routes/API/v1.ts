8ac04310b45101483215a2f9de61a739440de79d
kawinhill1112 <kawinhill1112@gmail.com>
2024-04-28 19:43:45 +0700

    feat(API/v1.ts): add Authentication middleware to API routes for improved security and authentication handling

--- src/routes/API/v1.ts
+++ src/routes/API/v1.ts
@@ -3,6 +3,7 @@ import Login from "../../controllers/API/public/Login";
 import Register from "../../controllers/API/public/Register";
 import Logout from "../../controllers/API/authenticated/Logout";
 import Me from "../../controllers/API/authenticated/Me";
+import Authentication from "../../middlewares/Authentication";
 
 const router = express.Router();
 
